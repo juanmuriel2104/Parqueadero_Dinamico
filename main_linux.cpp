2a1bb08bf6cd9c9c39808a485e9e1913ae905949
juanmuriel2104 <juanmuriel2104@gmail.com>
2026-05-19 22:49:36 -0500

    Obtener hora

--- main_linux.cpp
+++ main_linux.cpp
@@ -12,3 +12,14 @@
 
 #define PUERTO 5000
 #define BUFFER_SIZE 1024
+
+std::string obtenerHora() {
+    std::time_t t = std::time(nullptr);
+    std::tm* tm_local = std::localtime(&t);
+    std::ostringstream oss;
+    oss << std::setfill('0')
+        << std::setw(2) << tm_local->tm_hour << ":"
+        << std::setw(2) << tm_local->tm_min  << ":"
+        << std::setw(2) << tm_local->tm_sec;
+    return oss.str();
+}
