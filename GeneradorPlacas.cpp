23494697d4a5f330ac340f9bfdd88b0e223eb3aa
juanmuriel2104 <juanmuriel2104@gmail.com>
2026-05-19 20:03:18 -0500

    Se agrega cambio en el archivo

--- GeneradorPlacas.cpp
+++ GeneradorPlacas.cpp
@@ -3,3 +3,15 @@
 #include <ctime>
 #include <sstream>
 #include <iomanip>
+
+GeneradorPlacas::GeneradorPlacas(Parqueadero& parqueadero,
+                                 std::function<void(std::string, std::string, int, std::string)> callback)
+    : parqueadero(parqueadero), onEvento(callback) {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+}
+ 
+std::string GeneradorPlacas::generarPlacaAleatoria() {
+    // Formato colombiano: ABC-123
+    // Pool pequeño de letras para aumentar probabilidad de repetición (simula salidas)
+    const std::string letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const std::string digitos = "0123456789";
