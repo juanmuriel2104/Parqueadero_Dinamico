ab3c5e1fb8da495b0db4db6818d29cedf694e1cf
juanmuriel2104 <juanmuriel2104@gmail.com>
2026-05-20 00:41:12 -0500

    crear visualizador en python

---
+++ visualizador.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import socket
+import threading
+ 
+# ─────────────────────────────────────────────
+# Configuración de conexión
+# ─────────────────────────────────────────────
+HOST = "127.0.0.1"
+PORT = 5000
